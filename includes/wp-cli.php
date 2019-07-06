08f23ec539c6c50de702eb3ded96d85e8b5846ab
Michael Beckwith <tw2113@gmail.com>
2019-07-05 19:21:07 -0500

    a bunch of helper functions that should be abstract enough to handle all our data types, but only actually tested with steps

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -12,23 +12,84 @@ class ProcessFitBitData extends \WP_CLI_Command {
 
 	public $data = array();
 
+	private $path;
 
-	public function import( $args, $assoc_args ) {
-		$this->args       = $args;
-		$this->assoc_args = $assoc_args;
+	private $wpdb;
 
+	public function __construct() {
+		$this->path = WP_CONTENT_DIR . '/plugins/fitbit-data/';
 
+		global $wpdb;
+		$this->wpdb = $wpdb;
 	}
 
-	public function export( $args, $assoc_args ) {
+
+	public function run( $args, $assoc_args ) {
 		$this->args       = $args;
 		$this->assoc_args = $assoc_args;
+
+
+		$files = $this->get_files_by_data_type( $this->assoc_args['data_type'] );
+
+		$progress_bar = \WP_CLI\Utils\make_progress_bar(
+			'Processing ' . count( $files ) . ' files',
+			count( $files )
+		);
+		foreach ( $this->file_generator( $files ) as $f ) {
+			$fileparts = explode( '/', $f );
+
+			$file = $this->parse_data_file( $f );
+
+			foreach( $this->json_data_generator( $file ) as $item ) {
+				$this->insert( $item, $this->assoc_args['data_type'] );
+			}
+			$progress_bar->tick();
+		}
+		$progress_bar->finish();
+	}
+
+	private function get_files_by_data_type( $data_type = '' ) : array {
+		$paths = glob( $this->path . "data/user-site-export/$data_type-*.json" );
+		return $paths;
 	}
 
 	private function file_generator( $files ) {
 		foreach ( $files as $file ) {
 			yield $file;
 		}
+	}
+
+	private function parse_data_file( $file ) {
+		$data = json_decode( file_get_contents( $file ) );
+		\WP_CLI::LOG( "\nFound " . count( $data ) . ' records to insert' );
+
+		return $data;
+	}
+
+	private function json_data_generator( $data ) {
+		foreach ( $data as $datum ) {
+			yield $datum;
+		}
+	}
+
+	private function insert( $item, $data_type ) {
+
+		if ( '0' === $item->value ) {
+			return;
+		}
+
+		$formatted = date( 'Y-m-d H:i:s', strtotime( $item->dateTime ) );
+		$this->wpdb->insert(
+			$this->wpdb->prefix . 'fitbit_' . $data_type,
+			[
+				'dateTime' => $formatted,
+				$data_type => $item->value,
+			],
+			[
+				'%s',
+				'%s'
+			]
+		);
 	}
 }
 \WP_CLI::add_command( 'fitbit', __NAMESPACE__ . '\ProcessFitBitData' );
